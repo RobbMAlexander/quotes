9dd3c34f117bf532830ec31e7f7aea0afd21ae1d
KevLaMDev <lamarca.k@gmail.com>
2022-02-24 21:01:30 -0500

    lab09 mvp met

--- app/src/main/java/quotes/Quote.java
+++ app/src/main/java/quotes/Quote.java
@@ -6,10 +6,20 @@ public class Quote {
     String likes;
     String text;
 
+    public Quote(String author, String text) {
+        this.author = author;
+        this.text = text;
+    }
+
     public Quote(String[] tags, String author, String likes, String text) {
         this.tags = tags;
         this.author = author;
         this.likes = likes;
         this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return text + "\n-- " + author;
     }
 }
